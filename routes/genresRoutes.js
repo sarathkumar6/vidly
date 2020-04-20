1267c893aac0edc039cf9d3403640ce94a0c4e15
Sarath Kumar <nagarajsarath@gmail.com>
2020-04-19 20:35:06 -0400

    Update genresRoutes and customerRoutes

--- routes/genresRoutes.js
+++ routes/genresRoutes.js
@@ -1,21 +1,26 @@
 const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
-const { Genre, addGenresSchema, updateSchema} = require('../models/genre');
+const { Genre, addGenresSchema, updateSchema } = require("../models/genre");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const asyncMiddleware = require("../middleware/asyncMiddleware");
 
 // Get all the genres
-router.get("/", async (request, response) => {
+router.get("/", async (request, response, next) => {
+  throw new Error('Retrieving genres failed');
   const genres = await Genre.find().sort("name");
-  copyOfGenres = _.cloneDeep(genres);
-  response.send(genres);
+  return response.send(genres);
 });
 // Get a specific genre
 router.get("/:id", async (request, response) => {
-  const getTheGenre = await Genre.findOne({genreId: request.params.id});
-  return getTheGenre? response.send(getTheGenre) : response.status(404).send("The genre id is not found");
+  const getTheGenre = await Genre.findOne({ genreId: request.params.id });
+  return getTheGenre
+    ? response.send(getTheGenre)
+    : response.status(404).send("The genre id is not found");
 });
 // Add a genre
-router.post("/", async (request, response) => {
+router.post("/", auth, async (request, response) => {
   try {
     addGenresSchema.validate(request.body);
     let addGenre = new Genre({
@@ -30,33 +35,38 @@ router.post("/", async (request, response) => {
   }
 });
 // Update a Genre
-router.put("/", async (request, response) => {
-    // Useful doc: https://masteringjs.io/tutorials/mongoose/update
+router.put("/", auth, async (request, response) => {
+  // Useful doc: https://masteringjs.io/tutorials/mongoose/update
   try {
     updateSchema.validate(request.body);
-    const getTheGenre = await Genre.findOne({genreId: _.capitalize(request.body.genreType.substr(0, 3))});
+    const getTheGenre = await Genre.findOne({
+      genreId: _.capitalize(request.body.genreType.substr(0, 3))
+    });
     await getTheGenre.updateOne({
       genreType: request.body.genreType,
       movies: request.body.movies
     });
-    const updatedGenre = await Genre.findOne({genreId: _.capitalize(request.body.genreType.substr(0, 3))});
+    const updatedGenre = await Genre.findOne({
+      genreId: _.capitalize(request.body.genreType.substr(0, 3))
+    });
     response.send(updatedGenre);
   } catch (error) {
     return response.status(400).send(error.message);
   }
 });
 // Delete a Genre
-router.delete("/:id", async (request, response) => {
-  try {
-    await Genre.deleteOne({
+router.delete("/:id", [auth, admin], async (request, response) => {
+  // ToDo: Error Handling to throw Something failed seem to not work properly
+  await Genre.deleteOne(
+    {
       genreId: request.params.id
-    }, function callBack(err, result) {
-        if (result) response.send(result);
-        else if (err) new Error('Deleting a genre failed', err);
-    });
-  } catch (error) {
-    return response.status(400).send(error.message);
-  }
+    },
+    function callBack(err, result) {
+      if (result) response.send(result);
+      else if (err)
+        response.status(400).send(new Error("Deleting a genre failed", err));
+    }
+  );
 });
 
 module.exports = router;
